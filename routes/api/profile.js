5ec9662f310114903c4d552e9c64865ccdf8c0d2
Rohit Dubey <dubey22rohit@gmail.com>
2020-09-23 23:33:02 +0530

    Second Commit

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,10 +1,53 @@
 const express = require('express')
 const router = express.Router()
+const auth = require('../../middleware/auth')
+const User = require('../../models/User')
+const { check, validationResult } = require('express-validator')
+const Profile = require('../../models/User')
+//@route GET api/profile/me
+//@desc GET CURRENT USER's profile
+//@access PRIVATE
 
-//@route GET api/profile
-//@desc TEST route
-//@access PUBLIC
+router.get('/me', auth, async (req, res) => {
+    try {
+        const profile = await Profile.findOne({ user: req.user.id }).populate('user', ['name', 'avatar'])
+        if (!profile) {
+            return res.status(400).json({ msg: 'There is no profile associated with this user' })
+        }
+        res.json(profile)
+    } catch (err) {
+        console.err(err.message);
+        res.status(500).send('Server Error')
+    }
+})
 
-router.get('/', (req, res) => res.send('Profile Route'))
+//@route POST api/profile
+//@desc CREATE or UPDATE USER PROFILE
+//@access PRIVATE
+router.post('/', [auth, [
+    check('status', 'Status Required').not().isEmpty(),
+    check('skills', 'Skills are Required').not().isEmpty()
+]],
+    async (req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() })
+        }
+        const { company, website, location, bio, status, githubusername, skills, youtube, instagram, facebook, twitter, linkedin } = req.body;
+        //Build a profile object
+        const profileFields = {};
+        profileFields.user = req.user.id;
+        if (company) profileFields.company = company;
+        if (website) profileFields.website = website;
+        if (location) profileFields.location = location;
+        if (bio) profileFields.bio = bio;
+        if (status) profileFields.status = status;
+        if (githubusername) profileFields.githubusername = githubusername;
+        if (skills) {
+            profileFields.skills = skills.split(',').map(skill => skill.trim());
+        }
+        console.log(profileFields.skills)
+        res.send('Hello')
+    })
 
 module.exports = router;
